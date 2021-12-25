ad97bd5b7e69e48774bde94f36844bd026a4284a
Othniel Ussher <othnielussher16@gmail.com>
2021-12-25 20:15:43 +0000

    🥇 Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Doubles {}
+
+class Strings {
+  static String line1 =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+  static String line2 =
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+  static String line3 =
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
+  static const String about =
+      '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ''';
+}
+
+class Images {
+  static const String imagesPath = 'assets/images/';
+  static const String hero = imagesPath + 'hero.jpg';
+  static const String alicante = imagesPath + 'alicante.jpg';
+  static const String darEsSalaam = imagesPath + 'darEsSalaam.jpg';
+  static const String delhi = imagesPath + 'delhi.jpg';
+  static const String dortmund = imagesPath + 'dortmund.jpg';
+  static const String kinshasa = imagesPath + 'kinshasa.jpg';
+  static const String leHavre = imagesPath + 'leHavre.jpg';
+  static const String milan = imagesPath + 'milan.jpg';
+  static const String monteCarlo = imagesPath + 'monteCarlo.jpg';
+  static const String mumbai = imagesPath + 'mumbai.jpg';
+}
+
+class Palette {
+  static const Color green = Color(0xFF2E8664);
+  static const Color lightBlue = Color(0xFF0277bd);
+  static const Color red = Color(0xFFD10A24);
+  static const Color blue = Color(0xFF1D5192);
+  static const Color brown = Color(0xFF4C423E);
+  static const Color pink = Color(0xFFE57373);
+  static const Color navyBlue = Color(0xFF000080);
+  static const Color yellow = Color(0xFFF0D700);
+}
