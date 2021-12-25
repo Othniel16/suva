e6099dbe22650f68b6d12d1ad2464bfc6caf5a63
Othniel Ussher <othnielussher16@gmail.com>
2021-12-25 20:15:46 +0000

    🥇 Initial commit

---
+++ lib/exports.dart
@@ -0,0 +1,26 @@
+// pub
+export 'package:provider/provider.dart';
+export 'package:glass_kit/glass_kit.dart';
+export 'package:advanced_icon/advanced_icon.dart';
+export 'package:bordered_text/bordered_text.dart';
+export 'package:delayed_display/delayed_display.dart';
+export 'package:animated_text_kit/animated_text_kit.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+export 'package:concentric_transition/concentric_transition.dart';
+export 'package:linked_scroll_controller/linked_scroll_controller.dart';
+
+// local
+export 'package:suva/home.dart';
+export 'package:suva/constants.dart';
+export 'package:suva/widgets/dock_item.dart';
+export 'package:suva/widgets/project_box.dart';
+export 'package:suva/widgets/project_list.dart';
+export 'package:suva/sections/hero_section.dart';
+export 'package:suva/widgets/section_label.dart';
+export 'package:suva/widgets/project_title.dart';
+export 'package:suva/widgets/hover_detector.dart';
+export 'package:suva/state/section_provider.dart';
+export 'package:suva/sections/about_section.dart';
+export 'package:suva/sections/contact_section.dart';
+export 'package:suva/sections/project_section.dart';
+export 'package:suva/sections/widgets/section_header.dart';
