5546fe3bb8c4890803d5cc116e9e1abdb55e9527
Othniel Ussher <othnielussher16@gmail.com>
2022-01-05 18:49:43 +0000

    🌘 Add dark theme switching

--- lib/widgets/dock.dart
+++ lib/widgets/dock.dart
@@ -1,10 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:suva/exports.dart';
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
-class Dock extends StatelessWidget {
+class Dock extends StatefulWidget {
   const Dock({Key? key}) : super(key: key);
+
+  @override
+  State<Dock> createState() => _DockState();
+}
+
+class _DockState extends State<Dock> {
+  bool isLightTheme = true;
+  bool hovering = false;
+  static var lightTheme = const Icon(
+    CupertinoIcons.sun_max,
+    color: Colors.grey,
+  );
+
+  static var darkTheme = const Icon(
+    CupertinoIcons.moon,
+    color: Colors.grey,
+  );
+
+  var themeIcon = darkTheme;
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +38,19 @@ class Dock extends StatelessWidget {
                 alignment: Alignment.bottomCenter,
                 child: IntrinsicHeight(
                   child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10.0, horizontal: 8.0),
+                    padding: const EdgeInsets.all(10.0),
                     margin: const EdgeInsets.symmetric(vertical: 20.0),
                     decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16.0),
+                      color: ThemeProvider.themeOf(context).id == 'knight'
+                          ? const Color(0xFF161616)
+                          : Colors.white,
+                      borderRadius: BorderRadius.circular(20.0),
                       border: Border.all(color: Colors.grey.withOpacity(0.2)),
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.grey.withOpacity(0.1),
+                          color: ThemeProvider.themeOf(context).id == 'knight'
+                              ? Colors.transparent
+                              : Colors.grey.withOpacity(0.1),
                           offset: const Offset(0, 34),
                           spreadRadius: 10.0,
                           blurRadius: 20.0,
@@ -64,6 +87,29 @@ class Dock extends StatelessWidget {
                           ),
                           onTap: () => provider.changeSection = 2,
                         ),
+                        const SizedBox(width: 8.0),
+                        DockItem(
+                          label: 'Theme',
+                          icon: AnimatedCrossFade(
+                            duration: const Duration(milliseconds: 500),
+                            secondChild: lightTheme,
+                            firstChild: darkTheme,
+                            crossFadeState: isLightTheme
+                                ? CrossFadeState.showFirst
+                                : CrossFadeState.showSecond,
+                          ),
+                          onTap: () {
+                            ThemeProvider.controllerOf(context).nextTheme();
+                            setState(() {
+                              isLightTheme = !isLightTheme;
+                              if (isLightTheme) {
+                                themeIcon = darkTheme;
+                              } else {
+                                themeIcon = lightTheme;
+                              }
+                            });
+                          },
+                        ),
                         const SizedBox(width: 10.0),
                         VerticalDivider(
                             color: Colors.grey.withOpacity(0.3), width: 3.0),
@@ -75,6 +121,28 @@ class Dock extends StatelessWidget {
                             FontAwesomeIcons.envelope,
                             color: Colors.grey,
                           ),
+                        ),
+                        const SizedBox(width: 8.0),
+                        DockItem(
+                          label: 'GitHub',
+                          icon: const FaIcon(
+                            FontAwesomeIcons.github,
+                            color: Colors.grey,
+                          ),
+                          onTap: () {
+                            html.window.open(Urls.github, 'GitHub');
+                          },
+                        ),
+                        const SizedBox(width: 8.0),
+                        DockItem(
+                          label: 'LinkedIn',
+                          icon: const FaIcon(
+                            FontAwesomeIcons.linkedin,
+                            color: Colors.grey,
+                          ),
+                          onTap: () {
+                            html.window.open(Urls.github, 'GitHub');
+                          },
                         ),
                         const SizedBox(width: 8.0),
                         DockItem(
@@ -96,17 +164,6 @@ class Dock extends StatelessWidget {
                           ),
                           onTap: () {
                             html.window.open(Urls.twitter, 'Twitter');
-                          },
-                        ),
-                        const SizedBox(width: 8.0),
-                        DockItem(
-                          label: 'GitHub',
-                          icon: const FaIcon(
-                            FontAwesomeIcons.github,
-                            color: Colors.grey,
-                          ),
-                          onTap: () {
-                            html.window.open(Urls.github, 'GitHub');
                           },
                         ),
                         const SizedBox(width: 8.0),
